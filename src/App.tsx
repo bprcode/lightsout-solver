8cda24484b9671d4292a986b3705f86505840a02
bprcode <bprcode@gmail.com>
2025-04-07 14:21:30 -0700

    Scroll solution steps

--- src/App.tsx
+++ src/App.tsx
@@ -89,8 +89,8 @@ function LightBoard({
           key={i}
           className={`${
             bit
-              // ? 'bg-emerald-500 group-hover:bg-emerald-300 group-active:bg-teal-200 extra-light-edge'
-              ? 'illuminated group-hover:very-illuminated group-active:bg-teal-200'
+              ? // ? 'bg-emerald-500 group-hover:bg-emerald-300 group-active:bg-teal-200 extra-light-edge'
+                'illuminated group-hover:very-illuminated group-active:bg-teal-200'
               : 'bg-stone-800 group-hover:bg-stone-700 group-active:bg-stone-400 light-edge'
           } flex justify-center w-full h-full`}
         >
@@ -171,12 +171,14 @@ function stepDiff(
 function SolutionSteps({
   solution,
   size,
+  className = '',
 }: {
   solution: BitBoard[]
   size: BoardSize
+  className?: string
 }) {
   return (
-    <div>
+    <div className={className}>
       <h2 className="text-slate-200 mt-4">Solution steps:</h2>
       <div className="flex flex-wrap gap-4">
         {solution.map((s, i) => {
@@ -256,7 +258,7 @@ function App() {
     'px-3 py-1 mb-4 rounded-lg font-semibold h-10 relative '
 
   return (
-    <div className="max-w-3xl mx-auto mt-8  bg-zinc-800 text-slate-200 p-4 light-edge">
+    <div className="max-w-3xl mx-auto mt-8  bg-zinc-800 text-slate-200 p-4 light-edge flex flex-col">
       <div className="prose text-slate-200">
         <h1 className="text-slate-300 font-bold">Lights Out Solver</h1>
 
@@ -272,76 +274,88 @@ function App() {
         </p>
       </div>
 
-      <div className="flex gap-8 mt-8 w-fit">
-        <div className="flex flex-col">
-          <LightBoard
-            className="mb-4"
-            board={bitBoard}
-            size={boardSize}
-            onFlip={(r, c) => {
-              if (solution === null) {
-                setSolution(undefined)
-              }
-              setBitBoard(inputMode(bitBoard, boardSize, r, c))
-            }}
-          />
+      <div className="outline-4 outline-rose-500/10 mt-8 flex gap-8">
+        <div className="flex gap-8 w-fit bg-blue-400/10">
+          <div className="flex flex-col">
+            <LightBoard
+              className="mb-4"
+              board={bitBoard}
+              size={boardSize}
+              onFlip={(r, c) => {
+                if (solution === null) {
+                  setSolution(undefined)
+                }
+                setBitBoard(inputMode(bitBoard, boardSize, r, c))
+              }}
+            />
 
-          <button
-            disabled={workerThinking}
-            className={
-              workerThinking
-                ? baseButtonStyle + pressedStyle
-                : unpressedStyle + baseButtonStyle + 'w-full'
-            }
-            onClick={() => {
-              setWorkerThinking(true)
-              setSolution(undefined)
-              solveWorker?.postMessage({ bitBoard, boardSize })
-            }}
-          >
-            Solve
-          </button>
+            <button
+              disabled={workerThinking}
+              className={
+                workerThinking
+                  ? baseButtonStyle + pressedStyle
+                  : unpressedStyle + baseButtonStyle + 'w-full'
+              }
+              onClick={() => {
+                setWorkerThinking(true)
+                setSolution(undefined)
+                solveWorker?.postMessage({ bitBoard, boardSize })
+              }}
+            >
+              Solve
+            </button>
+          </div>
+          <div className="flex flex-col w-fit">
+            <button
+              className={`${
+                inputMode === togglePlus ? pressedStyle : unpressedStyle
+              } ${baseButtonStyle}`}
+              onClick={() => {
+                setInputMode(() => togglePlus)
+              }}
+            >
+              Toggle linked tiles
+            </button>
+            <button
+              className={`${
+                inputMode === toggleSingle ? pressedStyle : unpressedStyle
+              } ${baseButtonStyle}`}
+              onClick={() => setInputMode(() => toggleSingle)}
+            >
+              Toggle one at a time
+            </button>
+            <select
+              className="bg-zinc-700 px-4 py-2 light-edge mx-1"
+              value={boardSize}
+              onChange={e => {
+                setBoardSize(Number(e.target.value) as BoardSize)
+                setBitBoard(
+                  makeRandomBoard(Number(e.target.value) as BoardSize)
+                )
+                setSolution(undefined)
+              }}
+            >
+              <option value="2">2x2</option>
+              <option value="3">3x3</option>
+              <option value="4">4x4</option>
+              <option value="5">5x5</option>
+            </select>
+          </div>
         </div>
-        <div className="flex flex-col w-fit">
-          <button
-            className={`${
-              inputMode === togglePlus ? pressedStyle : unpressedStyle
-            } ${baseButtonStyle}`}
-            onClick={() => {
-              setInputMode(() => togglePlus)
-            }}
-          >
-            Toggle linked tiles
-          </button>
-          <button
-            className={`${
-              inputMode === toggleSingle ? pressedStyle : unpressedStyle
-            } ${baseButtonStyle}`}
-            onClick={() => setInputMode(() => toggleSingle)}
-          >
-            Toggle one at a time
-          </button>
-          <select
-            className="bg-zinc-700 px-4 py-2 light-edge mx-1"
-            value={boardSize}
-            onChange={e => {
-              setBoardSize(Number(e.target.value) as BoardSize)
-              setBitBoard(makeRandomBoard(Number(e.target.value) as BoardSize))
-              setSolution(undefined)
-            }}
-          >
-            <option value="2">2x2</option>
-            <option value="3">3x3</option>
-            <option value="4">4x4</option>
-            <option value="5">5x5</option>
-          </select>
+
+        <div className="bg-red-300/20 grow overflow-y-auto relative">
+          {solution === null && (
+            <h2 className="text-orange-300">This board cannot be solved.</h2>
+          )}
+          {solution && (
+            <SolutionSteps
+              className="absolute"
+              solution={solution}
+              size={boardSize}
+            />
+          )}
         </div>
       </div>
-
-      {solution === null && (
-        <h2 className="text-orange-300">This board cannot be solved.</h2>
-      )}
-      {solution && <SolutionSteps solution={solution} size={boardSize} />}
     </div>
   )
 }
